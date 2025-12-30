67f3ac4f8955b157ee965099c59d0e0a84f81041
Mohamed-Tagy <mohamed.120230167@ejust.edu.eg>
2025-12-30 17:22:56 +0200

    Receptionist Controller

--- hospital_management/controllers/receptionist_controller.py
+++ hospital_management/controllers/receptionist_controller.py
@@ -1,7 +1,11 @@
-class ReceptionistController:
+from controllers.base_controller import BaseController
+
+class ReceptionistController(BaseController):
+
     def __init__(self, patient_controller,
                  doctor_controller,
                  appointment_controller):
+        super().__init__()
         self.patient_controller = patient_controller
         self.doctor_controller = doctor_controller
         self.appointment_controller = appointment_controller
@@ -9,8 +13,11 @@ class ReceptionistController:
     def assign_patient_to_doctor(self, patient_id, doctor_id):
         patient = self.patient_controller.get(patient_id)
         doctor = self.doctor_controller.get(doctor_id)
-        if not patient or not doctor:
-            raise ValueError("Patient or Doctor not found")
+        if not patient:
+            raise ValueError("Patient not found")
+        if not doctor:
+            raise ValueError("Doctor not found")
         patient.assign_doctor(doctor_id)
         doctor.add_patient(patient_id)
-        return True
+
+        return "Patient assigned to doctor successfully"
