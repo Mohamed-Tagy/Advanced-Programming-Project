cb99d732dcab00f95a86df90ecd4463ea0fb130c
Mohamed-Tagy <mohamed.120230167@ejust.edu.eg>
2025-12-30 12:54:04 +0200

    Add Person, Staff, and Patient models

---
+++ hospital_management/models/person.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+class Person(ABC):
+    def __init__(self, person_id, name, age, gender, phone=None, email=None, address=None):
+        self.__person_id = person_id
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.phone = phone
+        self.email = email
+        self.address = address
+
+    @property
+    def person_id(self):
+        return self.__person_id
+    def update_contact(self, phone=None, email=None, address=None):
+        if phone is not None:
+            self.phone = phone
+        if email is not None:
+            self.email = email
+        if address is not None:
+            self.address = address
+
+    @abstractmethod
+    def to_dict(self):
+        pass
+
+    def __str__(self):
+        return f"{self.name} (ID: {self.__person_id})"
