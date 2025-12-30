37ebd8c396a9138b5e8e962bb18a0e7c3f2003e7
Mohamed-Tagy <mohamed.120230167@ejust.edu.eg>
2025-12-30 23:50:16 +0200

    Update nurse.py

--- hospital_management/models/nurse.py
+++ hospital_management/models/nurse.py
@@ -1,17 +1,1 @@
-from models.staff import Staff
-class Nurse(Staff):
-    def __init__(self, staff_id, name, age, gender, department,
-                 phone=None, email=None, address=None):
-        super().__init__(staff_id, name, age, gender, "Nurse", phone, email, address)
-        self.department = department
-        self.__assigned_patients = []
 
-    def assign_patient(self, patient_id):
-        self.__assigned_patients.append(patient_id)
-
-    def to_dict(self):
-        return {
-            "id": self.person_id,
-            "department": self.department,
-            "patients": self.__assigned_patients
-        }
